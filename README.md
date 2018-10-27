89233191d84a8a2ae8385a459d69a8ade3d6ee2a
va2577 <va2577@users.noreply.github.com>
2018-10-27 16:32:39 +0900

    css

--- README.md
+++ README.md
@@ -11,7 +11,11 @@ $ git clone https://github.com/va2577/an6474.git theme/an6474
 ## stylus
 
 ```
-$ npx stylus < src/css/style.styl > static/css/style.css
+$ npx stylus -I src/css/ < src/css/style-default.styl > static/css/style-default.css
+$ npx stylus -I src/css/ < src/css/style-dark.styl > static/css/style-dark.css
+$ npx stylus -I src/css/ < src/css/style-light.styl > static/css/style-light.css
 # or
-$ npx stylus src/css/style.styl --out static/css/style.css
+$ npx stylus -I src/css/ src/css/style-default.styl --out static/css/style-default.css
+$ npx stylus -I src/css/ src/css/style-dark.styl --out static/css/style-dark.css
+$ npx stylus -I src/css/ src/css/style-light.styl --out static/css/style-light.css
 ```
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,26 @@
-// keep
+/**
+ * script.js
+ */
+const getItem = k => {
+  if (!localStorage) return null
+  return localStorage.getItem(k)
+}
+
+const setItem = (k, v) => {
+  if (!localStorage) return
+  localStorage.setItem(k, v)
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+  const colors = ['default', 'dark', 'light']
+  const links = colors.map(x => document.getElementById(`link-${x}`))
+  const anchors = colors.map(x => document.getElementById(`anchor-${x}`))
+  const f = color => {
+    links.map(x => x.disabled = x.id.replace('link-', '') !== color)
+    setItem('color', color)
+  }
+  anchors.map(x => x.addEventListener('click', event => f(event.target.id.replace('anchor-', ''))))
+  const c1 = getItem('color')
+  const c2 = !c1 ? 'default' : c1
+  f(c2)
+})
